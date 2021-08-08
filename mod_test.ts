9a42da3203788fba151d571cf751a3755d63c3c1
Arghyadeep <arghyadeep.k@gmail.com>
2021-08-08 19:02:23 +0530

    UAdded test cases

--- mod_test.ts
+++ mod_test.ts
@@ -1,6 +1,22 @@
 import { assertEquals } from "./test_deps.ts";
 import { validateTLD } from "./mod.ts";
 
-Deno.test("Test with defaults", () => {
-  assertEquals(validateTLD("in"), true);
+Deno.test("Test with lowercase input", () => {
+  const input = "in";
+  assertEquals(validateTLD(input), true);
+});
+
+Deno.test("Test with uppercase input", () => {
+  const input = "IN";
+  assertEquals(validateTLD(input), true);
+});
+
+Deno.test("Test with unhappy path", () => {
+  const input = "zebra";
+  assertEquals(validateTLD(input), false);
+});
+
+Deno.test("Test with happy path", () => {
+  const input = "PHOTOGRAPHY";
+  assertEquals(validateTLD(input), true);
 });
